68128c0ac8ac49ec3e0279dc4005ba9d00fc19a6
s223880826 <168641205+s223880826@users.noreply.github.com>
2024-05-30 01:25:21 +1000

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ images = [
 @app.route('/')
 def index():
     url = random.choice(images)
-    return render_template('index.html', url=url)
+    return render_template('wonderful_adventure.html', url=url)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
